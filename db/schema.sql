36be1a2ca6eeea12aa8f9be55ad816f86d0837f3
kevincallaghan <kevincallaghan5@yahoo.com>
2023-05-29 12:51:14 -0500

    getting started

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS employeeTrack_db;
+CREATE DATABASE employeeTrack_db;
+USE employeeTrack_db;
+
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    departName VARCHAR(25)
+);
+
+CREATE TABLE employee (
+
+);
+
+CREATE TABLE role (
+
+);
